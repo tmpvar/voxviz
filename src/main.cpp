9dbcd49f72f308881c8adecf07cffaa43a17467e
Elijah Insua <tmpvar@gmail.com>
2018-07-14 10:16:29 -0700

    process grbl_sim output from stdin and move the tool to simulate cutting operation

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,12 @@
 #include <shaders/built.h>
 #include <iostream>
 
+#include <uv.h>
+
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <string.h>
+#include <queue>
 
 bool keys[1024];
 bool prevKeys[1024];
@@ -108,6 +111,81 @@ void apply_tool(Raytracer *raytracer, Compute *compute) {
   free(mem);
 }
 
+/* LIBUV JUNK*/
+typedef struct {
+  uv_buf_t buffer;
+  uv_write_t request;
+} write_req_t;
+
+uv_loop_t *loop;
+uv_pipe_t stdin_pipe;
+
+static void alloc_buffer(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf) {
+  buf->base = (char *)malloc(suggested_size);
+  memset(buf->base, 0, suggested_size);
+  buf->len = suggested_size;
+}
+
+
+#define MAX_LINE_LENGTH 512
+char current_line[MAX_LINE_LENGTH];
+unsigned int current_line_loc = 0;
+const char *line_splitter = " ";
+const char *line_prefix = "MACHINE_COORD ";
+queue<glm::vec3> tool_position_queue;
+
+void read_stdin(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buffer) {
+  if (nread > 0) {
+    cout << "read " << nread << " bytes" << endl;
+  }
+
+  if (buffer->base) {
+    char *base = buffer->base;
+    for (ssize_t i = 0; i < nread; i++) {
+
+      if (current_line_loc >= MAX_LINE_LENGTH) {
+        current_line_loc = MAX_LINE_LENGTH - 1;
+      }
+      if (base[i] == '\n') {
+        current_line[current_line_loc] = 0;
+        char *parse_pos = strstr(current_line, line_prefix);
+
+        if (parse_pos != NULL) {
+          char *pch = strtok(parse_pos + strlen(line_prefix), line_splitter);
+          glm::vec3 tool_pos;
+          tool_pos[0] = 0.0f;
+          float axis[3];
+          int axis_loc = 0;
+          while (pch != NULL) {
+            tool_pos[axis_loc] = atof(pch);
+            pch = strtok(NULL, line_splitter);
+            axis_loc++;
+            if (axis_loc > 3) {
+            tool_pos[axis_loc] = atof(pch);
+              break;
+            }
+          }
+
+          cout << "moving tool to (" << tool_pos.x << ", " << tool_pos.y << ", " << tool_pos.z << ")" << endl;
+
+          tool_position_queue.push(tool_pos);
+        }
+
+        memset(current_line, 0, MAX_LINE_LENGTH);
+        current_line_loc = 0;
+        continue;
+      }
+      else {
+        current_line[current_line_loc] = base[i];
+        current_line_loc++;
+      }
+
+    }
+    free(buffer->base);
+  }
+}
+/* LIBUV JUNK*/
+
 int main(void) {
   memset(keys, 0, sizeof(keys));
 
@@ -118,6 +196,13 @@ int main(void) {
   size_t total_voxels = dims[0] * dims[1] * dims[2];
   float dsquare = (float)d*(float)d;
   float camera_z = sqrtf(dsquare * 3.0f) * 1.5f;
+
+  // libuv junk
+  uv_loop_t* loop = uv_default_loop();
+  uv_pipe_init(loop, &stdin_pipe, 0);
+  uv_pipe_open(&stdin_pipe, 0);
+  uv_read_start((uv_stream_t*)&stdin_pipe, alloc_buffer, read_stdin);
+  // libuv junk
 
   GLFWwindow* window;
 
@@ -171,8 +256,8 @@ int main(void) {
 
   Raytracer *raytracer = new Raytracer(dims, compute->job);
 
-  for (float x = 0; x < 4; x++) {
-    for (float y = 0; y < 2; y++) {
+  for (float x = 0; x < 8; x++) {
+    for (float y = 0; y < 1; y++) {
       for (float z = 0; z < 4; z++) {
         raytracer->addVolumeAtIndex(x, y, z, VOLUME_DIMS, VOLUME_DIMS, VOLUME_DIMS);
       }
@@ -189,7 +274,7 @@ int main(void) {
   update_volumes(raytracer, compute, time);
 #endif
 
-  Volume *tool = raytracer->addVolumeAtIndex(5, 5, 5, 128, 512, 128);
+  Volume *tool = raytracer->addVolumeAtIndex(5, 5, 5, 64, 512, 64);
   compute->lock(compute->job.command_queues[0], tool->computeBuffer);
   compute->fill(
     "cylinder",
@@ -207,8 +292,8 @@ int main(void) {
     glDepthFunc(GL_LESS);
     glDepthMask(GL_TRUE);
     
-    //glCullFace(GL_BACK);
-    //glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glEnable(GL_CULL_FACE);
 
     glClearDepth(1.0);
     glClearColor(0.0, 0.0, 0.0, 1.0);
@@ -266,10 +351,15 @@ int main(void) {
         const GLFWvidmode *mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
         glfwSetWindowSize(window, mode->width, mode->height);
         glfwSetWindowPos(window, 0, 0);
+        glfwSetWindowMonitor(window, glfwGetPrimaryMonitor(), 0, 0, mode->width, mode->height, GLFW_DONT_CARE);
+        glViewport(0, 0, mode->width, mode->height);
         fullscreen = true;
       }
       else {
         glfwSetWindowSize(window, windowDimensions[0], windowDimensions[1]);
+        glfwSetWindowPos(window, 100, 100);
+        glfwSetWindowMonitor(window, NULL, 100, 100, windowDimensions[0], windowDimensions[1], GLFW_DONT_CARE);
+        glViewport(0, 0, windowDimensions[0], windowDimensions[0]);
         fullscreen = false;
       }
       prevKeys[GLFW_KEY_ENTER] = false;
@@ -286,7 +376,6 @@ int main(void) {
     camera->pitch((float)(delta[1] / 500.0));
     camera->yaw((float)(delta[0] / 500.0));
   
-
     if (glfwJoystickPresent(GLFW_JOYSTICK_1)) {
       int axis_count;
       const float *axis = glfwGetJoystickAxes(GLFW_JOYSTICK_1, &axis_count);
@@ -305,9 +394,16 @@ int main(void) {
       );
     }
 
-      camera->pitch(-axis[3]/20.0f);
-      camera->yaw(axis[2]/20.0f);
+    if (tool_position_queue.size() > 0) {
+      glm::vec3 new_tool_pos = glm::abs(tool_position_queue.front());
+      tool_position_queue.pop();
+      tool->position(
+        new_tool_pos.x * 10.0f,
+        new_tool_pos.z * 10.0f,
+        new_tool_pos.y * 10.0f
+      );
     }
+
 
     viewMatrix = camera->view();
 
@@ -322,14 +418,7 @@ int main(void) {
 #if RENDER_DYNAMIC == 1
     update_volumes(raytracer, compute, time);
 #endif
-    glFinish();
-
-    tool->position(
-      256.0f * 2.0f + sinf((float)time / 50.0f) * fmod(time, 3000.0f) / 5.0f,
-      512.0f - time / 100.0f,
-      256.0f * 2.0f + cosf((float)time / 50.0f) * fmod(time, 3000.0f) / 5.0f
-    );
-
+    glFlush();
 
     size_t total = raytracer->volumes.size();
     for (unsigned int i = 0; i < total; i++) {
@@ -343,6 +432,7 @@ int main(void) {
  
     time++;
 
+    uv_run(loop, UV_RUN_NOWAIT);
     glfwPollEvents();
   }
 
@@ -350,7 +440,9 @@ int main(void) {
   delete raytracer;
 
   Shaders::destroy();
-
+  uv_read_stop((uv_stream_t*)&stdin_pipe);
+  uv_stop(uv_default_loop());
+  uv_loop_close(uv_default_loop());
   glfwTerminate();
   return 0;
 }
