e598239d6205fec6fb7dfe12b106ec2ec0e933fc
Elijah Insua <tmpvar@gmail.com>
2019-01-13 14:52:42 -0700

    fix rendering of camera visual aid

--- experiments/lib/render/camera.js
+++ experiments/lib/render/camera.js
@@ -4,28 +4,25 @@ const cameraLookAt = require('../lookat')
 
 module.exports = drawCamera
 var a = mat3.create()
-function drawCamera(ctx, eye, origin) {
+function drawCamera(ctx, eye, origin, scale) {
   cameraLookAt(a, eye, origin)
-
+  scale = scale || 1
   const lineTo = ctx.lineTo.bind(ctx)
   const moveTo = ctx.moveTo.bind(ctx)
 
   ctx.save()
   ctx.beginPath()
-    tx(a, -5, -5, moveTo)
-    tx(a, +5, -5, lineTo)
-    tx(a, +5, +5, lineTo)
-    tx(a, -5, +5, lineTo)
-    tx(a, -5, -5, lineTo)
-    ctx.stroke()
-    ctx.fill()
+    tx(a, - 5 * scale, - 5 * scale, moveTo)
+    tx(a, + 5 * scale, - 5 * scale, lineTo)
+    tx(a, + 5 * scale, + 5 * scale, lineTo)
+    tx(a, - 5 * scale, + 5 * scale, lineTo)
+    tx(a, - 5 * scale, - 5 * scale, lineTo)
 
-  ctx.beginPath()
+
     tx(a, 0,  0, moveTo)
-    tx(a, +10, -5, lineTo)
-    tx(a, +10, +5, lineTo)
-    tx(a, 0,  0, moveTo)
-    ctx.stroke()
+    tx(a, 10 * scale, - 5 * scale, lineTo)
+    tx(a, 10 * scale, + 5 * scale, lineTo)
+    tx(a, 0, 0, moveTo)
     ctx.fill()
   ctx.restore()
 }
