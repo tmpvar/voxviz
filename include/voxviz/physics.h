23b9732592f20ddb247ce3282e60846f9fab5a74
Elijah Insua <tmpvar@gmail.com>
2019-09-21 11:04:07 -0700

    don't jump the gun on PBD

--- include/voxviz/physics.h
+++ include/voxviz/physics.h
@@ -1,17 +1,10 @@
 #pragma once
 
 
-#include <Simulation/SimulationModel.h>
-#include <Simulation/TimeStepController.h>
-
 namespace voxviz {
   class Physics {
     public:
-
-      PBD::SimulationModel simulationModel;
-      PBD::TimeStepController *timeStepController;
       Physics() {
-        this->timeStepController = new PBD::TimeStepController();
       }
   };
 }
